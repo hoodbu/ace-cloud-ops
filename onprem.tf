3013d6ab6827ea4126f406e076555d02d5eeec1d
Umair Hoodbhoy <hoodbu@gmail.com>
2021-02-09 12:32:50 -0500

    sourcing bashrc

--- onprem.tf
+++ onprem.tf
@@ -10,7 +10,8 @@ sudo apt upgrade -y
 sudo apt-get -y install traceroute unzip build-essential git gcc iperf3 apache2
 sudo apt autoremove
 sudo /etc/init.d/ssh restart
-echo 'PS1="\u@Call Center :~$"' >> /etc/bash.bashrc
+echo 'PS1="\u@Call Center :~$"' >> ~/.bashrc
+source ~/.bashrc
 EOF
 }
 
