85edbb481c1b502dae25e890a0ca1f983751311c
Umair Hoodbhoy <hoodbu@gmail.com>
2021-02-09 11:14:04 -0500

    persist

--- gcp-vm-config/gcp_bootstrap.sh
+++ gcp-vm-config/gcp_bootstrap.sh
@@ -4,5 +4,4 @@
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo echo 'ubuntu:Password123!' | /usr/sbin/chpasswd
 sudo /etc/init.d/ssh restart
-PS1="\u@BU1 Analytics :~$"
 echo 'PS1="\u@BU1 Analytics :~$"' >> ~/.bashrc
