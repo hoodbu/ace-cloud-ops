69335fc470112401321604d149e2eba24ca784c9
Umair Hoodbhoy <hoodbu@gmail.com>
2021-02-09 17:25:26 -0500

    gcp vm cleanup

--- gcp-vm-config/gcp_bootstrap.sh
+++ gcp-vm-config/gcp_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # allow Linux access by password 
-sudo hostnamectl set-hostname "BU1-Analytics"
+sudo hostnamectl set-hostname ${name}
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo echo 'ubuntu:Password123!' | /usr/sbin/chpasswd
+sudo echo 'ubuntu:${password}' | /usr/sbin/chpasswd
 sudo /etc/init.d/ssh restart
--- gcp.tf
+++ gcp.tf
@@ -1,14 +1,18 @@
 // Test GCP VM
 
 data "template_file" "gcp-init" {
-  # template = file("${path.module}/gcp-vm-config/gcp_bootstrap.sh")
-  template = <<EOF
+  template = file("${path.module}/gcp-vm-config/gcp_bootstrap.sh")
+  vars = {
+    name     = "BU1-Analytics"
+    password = var.ace_password
+  }
+/*   template = <<EOF
 #!/bin/bash
 sudo hostnamectl set-hostname "BU1-Analytics"
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo echo 'ubuntu:${var.ace_password}' | /usr/sbin/chpasswd
 sudo /etc/init.d/ssh restart
-EOF
+EOF */
 }
 
 resource "google_compute_firewall" "gcp-comp-firewall" {
