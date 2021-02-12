03a6fcbe6807b4798cbdc2c7ec1f08c4e5cf1613
Umair Hoodbhoy <hoodbu@gmail.com>
2021-02-11 19:08:49 -0500

    rbac interpolation

--- rbac.tf
+++ rbac.tf
@@ -6,14 +6,14 @@ resource "aviatrix_rbac_group" "rbac_group_100" {
 
 ### Permission Group to Access Account Attachment 
 resource "aviatrix_rbac_group_access_account_attachment" "rbac_group_access_account_attachment_1" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     access_account_name = "all"
 }
 
 
 ### Permission Group to User Attachment
 resource "aviatrix_rbac_group_user_attachment" "rbac_group_user_attachment_1" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     user_name = "pod1"
 }
 
@@ -27,72 +27,72 @@ resource "aviatrix_account_user" "account_user_1" {
 
 ### Permission Group to Permission Set Attachment
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_1" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_dashboard_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_2" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_accounts_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_3" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_gateway_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_4" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_tgw_orchestrator_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_5" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_transit_network_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_6" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_firewall_network_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_7" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_cloud_wan_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_8" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_peering_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_9" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_site2cloud_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_10" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_openvpn_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_11" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_security_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_12" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_useful_tools_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_13" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_troubleshoot_write"
 }
 
 resource "aviatrix_rbac_group_permission_attachment" "rbac_group_permission_attachment_14" {
-    group_name = "local-students"
+    group_name = aviatrix_rbac_group.rbac_group_100.group_name
     permission_name = "all_write"
 }
 
