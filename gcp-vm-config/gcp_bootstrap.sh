bed0765810f3bb064d5511c9d418d55d2e78f9c3
Umair Hoodbhoy <hoodbu@gmail.com>
2021-02-21 14:24:28 -0500

    boot-strap touchup

--- gcp-vm-config/gcp_bootstrap.sh
+++ gcp-vm-config/gcp_bootstrap.sh
@@ -4,4 +4,9 @@
 sudo hostnamectl set-hostname ${name}
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo echo 'ubuntu:${password}' | /usr/sbin/chpasswd
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get -y install traceroute unzip build-essential git gcc iperf3 apache2 net-tools
+sudo apt autoremove
 sudo /etc/init.d/ssh restart
+echo “<html><h1>The web server is up</h1></html>” > index.html 
