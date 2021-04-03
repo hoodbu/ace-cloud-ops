3b5bb8b95269c05e2d69f98cd9e5d7590a1df27a
Umair Hoodbhoy <hoodbu@gmail.com>
2021-04-03 14:39:51 -0400

    debug

--- main.tf
+++ main.tf
@@ -247,7 +247,6 @@ output "firewall_public_ip" {
 
 ########################################################################
 
-/*
 resource "aviatrix_transit_external_device_conn" "s2c-onprem-dc" {
     vpc_id = module.aws_transit_1.vpc.vpc_id
     connection_name = "ACE-ONPREM-DC"
@@ -263,4 +262,3 @@ resource "aviatrix_transit_external_device_conn" "s2c-onprem-dc" {
     remote_tunnel_cidr = "169.254.74.129/30"
     custom_algorithms = false
 }
-*/
