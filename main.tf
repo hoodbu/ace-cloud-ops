1d4b48669cb1e175ac9a0f531d564bccf698878d
Umair Hoodbhoy <hoodbu@gmail.com>
2021-04-27 16:50:16 -0400

    testing excluded_cidrs in new version of mc-transit-peering module. Adding depends_on for transit_firenet_policy

--- main.tf
+++ main.tf
@@ -190,17 +190,24 @@ module "transit-peering" {
     module.azure_transit_1.transit_gateway.gw_name,
     module.aws_transit_1.transit_gateway.gw_name
   ]
+
+  excluded_cidrs = [
+    "0.0.0.0/0",
+  ]
+
 }
 
 # Transit FireNet Spoke Inspection Policy
 resource "aviatrix_transit_firenet_policy" "transit_firenet_policy_1" {
   transit_firenet_gateway_name = var.aws_transit1_name
   inspected_resource_name      = "SPOKE:${var.aws_spoke1_name}"
+  depends_on                   = [module.aws_transit_1]
 }
 
 resource "aviatrix_transit_firenet_policy" "transit_firenet_policy_2" {
   transit_firenet_gateway_name = var.aws_transit1_name
   inspected_resource_name      = "SPOKE:${var.aws_spoke2_name}"
+  depends_on                   = [module.aws_transit_1]
 }
 
 
