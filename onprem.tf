e57ad12cf9328a6c002f53bd08664626aa80306b
Umair Hoodbhoy <hoodbu@gmail.com>
2021-12-15 10:44:35 -0500

    debug interpolation

--- onprem.tf
+++ onprem.tf
@@ -72,10 +72,10 @@ resource "aws_security_group" "ace-onprem-partner-sg" {
 resource "aws_eip_association" "eip_assoc" {
   provider = aws.west2
   instance_id   = aws_instance.ace-onprem-partner-csr.id
-  allocation_id = aws_eip.aws-onprem-partner-csr-eip.id
+  allocation_id = aws_eip.ace-onprem-partner-csr-eip.id
 }
 
-resource "aws_eip" "aws-onprem-partner-csr-eip" {
+resource "aws_eip" "ace-onprem-partner-csr-eip" {
   provider = aws.west2
   vpc = true
 }
@@ -110,7 +110,7 @@ resource "aws_instance" "ace-onprem-partner-csr" {
     ios-config-1160 = "exit"
     ios-config-1165 = "crypto ikev2 profile OnPrem-Aviatrix"
     ios-config-1170 = "match identity remote address ${module.gcp_spoke_1.spoke_gateway.eip} 255.255.255.255"
-    ios-config-1175 = "identity local address ${aws_eip.aws-onprem-partner-csr-eip}"
+    ios-config-1175 = "identity local address $aws_eip.ace-onprem-partner-csr-eip"
     ios-config-1180 = "authentication remote pre-share"
     ios-config-1185 = "authentication local pre-share"
     ios-config-1190 = "keyring local OnPrem-Aviatrix"
