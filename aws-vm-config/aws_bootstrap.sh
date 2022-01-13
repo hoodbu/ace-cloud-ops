98de6eaedcf149a418ced930af956965e373648a
Umair Hoodbhoy <hoodbu@gmail.com>
2022-01-12 16:58:29 -0800

    enable passwd auth on aws test instances

--- aws-vm-config/aws_bootstrap.sh
+++ aws-vm-config/aws_bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo hostnamectl set-hostname ${name}
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo echo 'ubuntu:${var.ace_password}' | /usr/sbin/chpasswd
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get -y install traceroute unzip build-essential git gcc hping3 apache2 net-tools
