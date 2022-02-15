ad9bb123554223b81779cc7d23fa04be04ea0b6e
Umair Hoodbhoy <hoodbu@gmail.com>
2022-02-15 10:11:15 -0500

    v5.0.0 of Transit FireNet module

--- versions.tf
+++ versions.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     aviatrix = {
-      source = "AviatrixSystems/aviatrix"
-      # version = "~> 2.20.1"
+      source  = "AviatrixSystems/aviatrix"
       version = "2.21.0-6.6.ga"
 
     }
