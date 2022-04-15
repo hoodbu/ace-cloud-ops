373c92326b58cae165fdc6a3351971067cd284c9
hoodbu <38543715+hoodbu@users.noreply.github.com>
2022-04-15 16:45:38 -0400

    Update firenet.tf

--- firenet.tf
+++ firenet.tf
@@ -6,6 +6,30 @@ module "aws_firenet_1" {
   firewall_image                       = var.aws_firewall_image
   firewall_image_version               = var.aws_firewall_image_version
   keep_alive_via_lan_interface_enabled = true
+  custom_fw_names = ["ACE-FW"]
+  user_data_1     = local.fw1
+}
+
+locals {
+  fw1 = templatefile("fortigate_bootstrap.tpl", {
+    name     = "ACE-FW"
+    password = var.ace_password
+    }
+  )
+}
+
+data "aviatrix_firenet_vendor_integration" "ven_int" {
+  vpc_id        = module.mc_transit_aws.vpc.vpc_id
+  instance_id   = module.mc_firenet_aws.aviatrix_firewall_instance[0].instance_id
+  vendor_type   = "Fortinet FortiGate"
+  public_ip     = module.mc_firenet_aws.aviatrix_firewall_instance[0].public_ip
+  username      = "ACE"
+  firewall_name = module.mc_firenet_aws.aviatrix_firewall_instance[0].firewall_name
+  save          = true
+  api_token     = "gnk1dQkGpg77jQ6d6r543Qqc05Q5p3"
+  depends_on = [
+    module.mc_firenet_aws
+  ]
 }
 
 # Transit FireNet Spoke Inspection Policy
