00e8465935d204a94dad823e072f30455100880b
Umair Hoodbhoy <hoodbu@gmail.com>
2022-05-12 13:15:09 -0700

    remove potentially redundant data source

--- onprem.tf
+++ onprem.tf
@@ -137,14 +137,14 @@ resource "aws_instance" "ace-onprem-partner-csr" {
   ]
 }
 
-data "aws_instance" "ace-onprem-partner-csr" {
-  provider = aws.west2
-  filter {
-    name   = "tag:Name"
-    values = ["ace-onprem-partner-csr"]
-  }
-  depends_on = [aws_instance.ace-onprem-partner-csr]
-}
+# data "aws_instance" "ace-onprem-partner-csr" {
+#   provider = aws.west2
+#   filter {
+#     name   = "tag:Name"
+#     values = ["ace-onprem-partner-csr"]
+#   }
+#   depends_on = [aws_instance.ace-onprem-partner-csr]
+# }
 
 data "aws_route_table" "ace-onprem-partner-rtb" {
   provider   = aws.west2
@@ -156,8 +156,9 @@ resource "aws_route" "ace-onprem-mapped-route" {
   provider               = aws.west2
   route_table_id         = data.aws_route_table.ace-onprem-partner-rtb.id
   destination_cidr_block = "192.168.1.0/24"
-  network_interface_id   = data.aws_instance.ace-onprem-partner-csr.network_interface_id
-  depends_on             = [module.ace-onprem-partner-vpc, aws_instance.ace-onprem-partner-csr]
+  # network_interface_id   = data.aws_instance.ace-onprem-partner-csr.network_interface_id
+  network_interface_id = aws_instance.ace-onprem-partner-csr.network_interface_id
+  depends_on           = [module.ace-onprem-partner-vpc, aws_instance.ace-onprem-partner-csr]
 }
 
 module "ace-onprem-ubu" {
