e8149ecf8efaedbfdf6b01bda6c061eee22f48f0
Umair Hoodbhoy <hoodbu@gmail.com>
2022-05-13 10:58:31 -0700

    cosmetic

--- onprem.tf
+++ onprem.tf
@@ -137,15 +137,6 @@ resource "aws_instance" "ace-onprem-partner-csr" {
   ]
 }
 
-# data "aws_instance" "ace-onprem-partner-csr" {
-#   provider = aws.west2
-#   filter {
-#     name   = "tag:Name"
-#     values = ["ace-onprem-partner-csr"]
-#   }
-#   depends_on = [aws_instance.ace-onprem-partner-csr]
-# }
-
 data "aws_route_table" "ace-onprem-partner-rtb" {
   provider   = aws.west2
   subnet_id  = module.ace-onprem-partner-vpc.public_subnets[0]
